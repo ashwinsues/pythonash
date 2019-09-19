d3a9596a60cfed0d1dced986ed3b8e97b1645dff
ashwiwnsues <ashwiwnsuresh11@gmail.com>
2019-09-19 10:05:51 +0530

    passed loop

--- large.py
+++ large.py
@@ -1,6 +1,13 @@
 a=int(input("enter the first number"))
 b=int(input("enter the second number"))
+c=int(input("enter the third number"))
 if(a>b):
     print(a)
+elif(a>c):
+    print(a)
+elif(b>a):
+    print(b)
+elif(b>c):
+    print(b)
 else:
-    print(b)  
+    print(c)
